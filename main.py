06d8c9bbdeb9f0c866bf8f86fb078538aa3a5f4f
Cesar Ilharco Magalhaes <cesarim22@gmail.com>
2015-11-09 00:07:19 +0200

    Implemented RMCAT evaluation test 5.1

--- main.py
+++ main.py
@@ -4,30 +4,58 @@ from nada import NadaSender, NadaReceiver
 from link_simulator import LinkSimulator
 
 
-def test_constant_capacity(sender, receiver):
-	CAPACITY_KBPS = 1500.0
-	RUNNING_TIME_MS = 10 * 1000.0
-	link_simulator = LinkSimulator(CAPACITY_KBPS)
+def __plot(receiver, times_ms, capacities_kbps):
+	def ms_to_s(t):
+		return t/1000
 
-	now_ms = 0.0
-	while now_ms < RUNNING_TIME_MS:
-		packet = sender.create_packet()
-		link_simulator.send_packet(packet)
-		if packet.arrival_time_ms is not None:
-			receiver.receive_packet(packet)
-		feedback = receiver.get_feedback()
-		if feedback is not None:
-			sender.receive_feedback(feedback)
-		now_ms = packet.send_time_ms
+	plt.subplot(311)
+	plt.plot(map(ms_to_s, receiver.time_ms), receiver.receiving_rates_kbps, label='receiving rate')
+	plt.plot(map(ms_to_s, [0.0]+times_ms), capacities_kbps[0:1]+capacities_kbps, label='link capacity', drawstyle='steps')
+	plt.ylabel('bitrate (kbps)', fontsize=16)
 
-	plt.figure(1)
-	plt.subplot(211)
-	plt.plot(receiver.time_ms, [CAPACITY_KBPS] * len(receiver.time_ms), label="link capacity")
-	plt.plot(receiver.time_ms, receiver.receiving_rates_kbps, label="receiving rate")
+	plt.subplot(312)
+	plt.plot(map(ms_to_s, receiver.time_ms), receiver.delay_signals_ms, label='delay signals')
+	plt.ylabel('delay (ms)', fontsize=16)
+
+	plt.subplot(313)
+	plt.plot(map(ms_to_s, receiver.time_ms), map(lambda x: 100.0*x, receiver.loss_ratios), label='loss ratio')
+	plt.ylabel('packet loss %', fontsize=16)
+	plt.xlabel('time (s)', fontsize=16)
 	plt.show()
+
+
+def __test_single_flow(sender, receiver, times_ms, capacities_kbps):
+
+	link_simulator = LinkSimulator(None)
+	now_ms = 0.0
+
+	for i in range(len(capacities_kbps)):
+		link_simulator.capacity_kbps = capacities_kbps[i]
+		end_time_ms = times_ms[i]
+		while now_ms < end_time_ms:
+			packet = sender.create_packet()
+			link_simulator.send_packet(packet)
+			if packet.arrival_time_ms is not None:
+				receiver.receive_packet(packet)
+			feedback = receiver.get_feedback()
+			if feedback is not None:
+				sender.receive_feedback(feedback)
+			now_ms = packet.send_time_ms
+
+	__plot(receiver, times_ms, capacities_kbps)
+
+
+def rmcat_evaluation_1(sender, receiver):
+	capacities_kbps = [1000.0, 2500.0, 600.0, 1000.0]
+	times_ms = map(lambda x:1000.0*x, [40, 60, 80, 99])
+	__test_single_flow(sender, receiver, times_ms, capacities_kbps)
+
+
+def test_constant_capacity(sender, receiver):
+	__test_single_flow(sender, receiver, [30 * 1000.0], [2500.0])
 
 
 if __name__ == '__main__':
     nada_sender = NadaSender()
     nada_receiver = NadaReceiver()
-    test_constant_capacity(nada_sender, nada_receiver)
+    rmcat_evaluation_1(nada_sender, nada_receiver)

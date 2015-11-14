0b42560df792fe883280f6d6906e18d074aea2f8
Cesar Ilharco Magalhaes <cesarim22@gmail.com>
2015-11-14 18:30:41 +0200

    Python3 compatible

--- evaluation_tests.py
+++ evaluation_tests.py
@@ -6,25 +6,22 @@ from link_simulator import LinkSimulator
 Evaluation tests for congestion control algorithms.
 """
 
-def ms_to_s(time_ms): return time_ms/1000.0
-def s_to_ms(time_s): return time_s*1000.0
-
 def __plot(receiver, times_ms, capacities_kbps):
     """
     Plot results, called at the end of a simulation.
     """
     plt.subplot(311)
-    plt.plot(map(ms_to_s, receiver.time_ms), receiver.receiving_rates_kbps, label='receiving rate')
-    plt.plot(map(ms_to_s, [0.0]+times_ms), capacities_kbps[0:1]+capacities_kbps,
+    plt.plot([time_ms/1000.0 for time_ms in receiver.time_ms], receiver.receiving_rates_kbps, label='receiving rate')
+    plt.plot([time_ms/1000.0 for time_ms in [0.0]+times_ms], capacities_kbps[0:1]+capacities_kbps,
              label='link capacity', drawstyle='steps')
     plt.ylabel('bitrate (kbps)', fontsize=16)
 
     plt.subplot(312)
-    plt.plot(map(ms_to_s, receiver.time_ms), receiver.delay_signals_ms, label='delay signal')
+    plt.plot([time_ms/1000.0 for time_ms in receiver.time_ms], receiver.delay_signals_ms, label='delay signal')
     plt.ylabel('delay (ms)', fontsize=16)
 
     plt.subplot(313)
-    plt.plot(map(ms_to_s, receiver.time_ms), [100.0*ratio for ratio in receiver.loss_ratios],
+    plt.plot([time_ms/1000.0 for time_ms in receiver.time_ms], [100.0*ratio for ratio in receiver.loss_ratios],
              label='loss ratio')
     plt.ylabel('packet loss %', fontsize=16)
     plt.xlabel('time (s)', fontsize=16)
@@ -60,7 +57,7 @@ def rmcat_evaluation_1(sender, receiver, jitter):
     https://tools.ietf.org/html/draft-ietf-rmcat-eval-test-02#section-5.1
     """
     capacities_kbps = [1000.0, 2500.0, 600.0, 1000.0]
-    times_ms = map(s_to_ms, [40, 60, 80, 99])
+    times_ms =  [40000.0, 60000.0, 80000.0, 99000.0]
     __test_single_flow(sender, receiver, times_ms, capacities_kbps, jitter)
 
 
